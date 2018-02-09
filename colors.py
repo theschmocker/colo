ee4868505cabbb01e10100b7183c721beda6f20b
theschmocker <jacobcs1996@gmail.com>
2018-02-09 11:47:52 -0600

    added some comments and todos

--- colors.py
+++ colors.py
@@ -89,11 +89,18 @@ def RGBtoHSL(rgb):
 
     return hsl
 
+# Adapted from https://en.wikipedia.org/wiki/HSL_and_HSV#Converting_to_RGB
 def HSLtoRGB(hsl):
+    #C
     chroma = (1 - abs(2 * hsl['l'] - 1)) * hsl['s']
+
+    #H'
     hue = hsl['h'] / 60
+
     x = chroma * (1 - abs((hue % 2) - 1))
     intermediateRGB = {}
+
+    # There's gotta be a better way to do this...
     if 0 <= hue and hue <= 1:
         intermediateRGB = {'r': chroma, 'g': x, 'b': 0}
     elif 1 <= hue and hue <= 2:
@@ -117,13 +124,19 @@ def HSLtoRGB(hsl):
 
 try:
     hex_color = sys.argv[1]
+
 except:
+    # TODO: adapt this to accept other color formats 
     print('Please provide a hex color as an argument. If you did, and are seeing this message, try removing the "#".')
     exit()
+
 if len(hex_color) != 3 and len(hex_color) != 6:
     print('Error: Malformed color.')
     exit()
 rgb = hexToRGB(hex_color)
+
+# TODO: Build a pretty printing system. In this format, this data is only useful for calculations
+# TODO: Get rid of this nonsense. It's only for testing
 print("Hex Color: #%s" % hex_color)
 print("RGB representation: %s" % rgb)
 print("HSL representation: %s" % RGBtoHSL(rgb))
