b0e27bb7aa8d0afdb63b6a8fda4a54b8fd95451c
theschmocker <jacobcs1996@gmail.com>
2018-02-11 22:18:59 -0600

    moved more code into appropriate modules. this file will have a rewrite soon

--- colors/colors.py
+++ colors/colors.py
@@ -4,44 +4,6 @@ import sys
 
 
 # Adapted from https://en.wikipedia.org/wiki/HSL_and_HSV#Converting_to_RGB
-def HSLtoRGB(hsl):
-    #C
-    chroma = (1 - abs(2 * hsl['l'] - 1)) * hsl['s']
-
-    #H'
-    hue = hsl['h'] / 60
-
-    x = chroma * (1 - abs((hue % 2) - 1))
-    intermediateRGB = {}
-
-    # There's gotta be a better way to do this...
-    if 0 <= hue and hue <= 1:
-        intermediateRGB = {'r': chroma, 'g': x, 'b': 0}
-    elif 1 <= hue and hue <= 2:
-        intermediateRGB = {'r': x, 'g': chroma, 'b': 0}
-    elif 2 <= hue and hue <= 3:
-        intermediateRGB = {'r': 0, 'g': chroma, 'b': x}
-    elif 3 <= hue and hue <= 4:
-        intermediateRGB = {'r': 0, 'g': x, 'b': chroma}
-    elif 4 <= hue and hue <= 5:
-        intermediateRGB = {'r': x, 'g': 0, 'b': chroma}
-    elif 5 <= hue and hue <= 6:
-        intermediateRGB = {'r': chroma, 'g': 0, 'b': x}
-    else:
-        print('Something went very, very wrong')
-        print('value of "hue": ' + hue)
-        exit(1)
-
-    m = hsl['l'] - 0.5 * chroma
-    
-    rgb = {}
-
-    rgb['r'] = 255 * (intermediateRGB['r'] + m)
-    rgb['g'] = 255 * (intermediateRGB['g'] + m)
-    rgb['b'] = 255 * (intermediateRGB['b'] + m)
-
-    return rgb
-
 try:
     hex_color = sys.argv[1]
 
